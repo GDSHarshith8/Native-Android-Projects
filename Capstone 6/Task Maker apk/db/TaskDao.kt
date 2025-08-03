8749e19b32b52640895b31dbefcd129cd1118975
Harshith <97158803+GDSHarshith8@users.noreply.github.com>
2025-08-03 20:34:59 +0530

    Create TaskDao.kt

---
+++ Capstone 6/Task Maker apk/db/TaskDao.kt
@@ -0,0 +1,26 @@
+package com.taskmaker.db
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.taskmaker.presentation.Task
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    fun getAllTasks(): Flow<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertTask(task: Task)
+
+    @Delete
+    suspend fun deleteTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+}
